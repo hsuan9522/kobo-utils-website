497854083282ef96d6330c2eede77972e39fe258
Hsuan <jh558125@gmail.com>
2025-11-21 13:44:02 +0100

    [Feat] add svg logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
     server: {
         port: 3000,
     },
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths(), svgr()],
 })
