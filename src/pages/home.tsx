dd7f077704353de82f585830926571cbd4b4e9f1
Hsuan <jh558125@gmail.com>
2025-11-21 13:52:33 +0100

    [Feat] home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,39 @@
+import Logo from '@/assets/logo.svg?react'
+import { TextTyping } from '@/components/textTyping'
+import { UploadField } from '@/components/uploadField'
+import { chakra, Flex } from '@chakra-ui/react'
+import { useNavigate } from 'react-router'
+
 const Home = () => {
-    return <div>home</div>
+    const navigate = useNavigate()
+    const uploadSuccess = () => {
+        navigate('/calendar')
+    }
+
+    return (
+        <>
+            <Flex
+                w="full"
+                h="full"
+                align="center"
+                justifyContent="center"
+                gap="4"
+                pb="70px"
+                direction="column"
+            >
+                <chakra.svg fill="gray.600" width="full" height="300px">
+                    <Logo />
+                </chakra.svg>
+                <TextTyping
+                    text="Upload your .sqlite file to get started"
+                    animation={true}
+                ></TextTyping>
+                <Flex justifyContent="center">
+                    <UploadField showFile={false} successCallback={uploadSuccess}></UploadField>
+                </Flex>
+            </Flex>
+        </>
+    )
 }
 
 export default Home
